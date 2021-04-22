39cfba8c24b6439ef10e7893667bf64701930dcd
Pac-man <isidluca@ucm.es>
2021-04-22 10:42:02 +0200

    trabajo avanzado en el laboratorio

--- Pac-man/Program.cs
+++ Pac-man/Program.cs
@@ -38,8 +38,6 @@ namespace Pac_man
             {
                 return (c is Coor) && this == (Coor)c;
             }
-
-
         }
 
         class Tablero
@@ -67,61 +65,24 @@ namespace Pac_man
 
             public Tablero(string file)
             {
-                file = ""; ; //string que busca el nivel al que se quiere acceder
+                char[] c =LecturaDeFichero(file);
 
-                StreamReader levels;
+                int columnas = Columnas(c);
 
-                levels = new StreamReader(file);
-
-                //Variables del tamaño del nivel para la creación de "Tablero"
-                int filas = 0;
-                int columnas;
-
-                //String auxiliar para la lectura del nive
-                string end = "";
-
-                //Booleano para el control del bucle
-                bool finish = false;
-
-                //El bucle va leyendo cada fila del archivo en "end" y la suma a "file", cuando end encuentre "" querra decir que no hay más lineas en el archivo
-                while (!finish)
-                {
-                    end = levels.ReadLine();
-
-                    file += end;
-                    file += ";";
-
-                    //Cada linea que lee la suma a filas para calcular el tamaño del array "Tablero"
-                    filas++;
-
-                    if (end == "")
-                    {
-                        finish = true;
-                    }
-                }
-
-                levels.Close();
-
-                //Creo un array auxiliar para calcular las columnas
-                char[] c;
-
-                c = file.ToCharArray();
-
-                //Este bucle recorre el array auxiliar "c" y suma uno a "columnas" y cuando encuentra el primer ";" significa que ese es el tamaño de columnas para crear "Tablero"
-                columnas = 0;
-                bool tamaño = false;
-                while (!tamaño)
-                {
-                    if (c[columnas] == ';') { tamaño = true; }
-                    else { columnas++; }
-                }
-
+                int filas = Filas(c);
                 
                 //Creación del tablero
                 cas = new Tablero.Casilla[filas,columnas];
 
                 //Creacion del array de personajes
-                pers = new Personaje[4];
+                pers = new Tablero.Personaje[4];
+
+                for (int j = 0; j < pers.Length; j++)
+                {
+                    pers[j].dir = new Coor();
+                    pers[j].pos = new Coor();
+                    pers[j].ini = new Coor();
+                }
 
                 //Direccion inicial de pac-man
                 pers[0].dir.fil = 1;
@@ -134,7 +95,7 @@ namespace Pac_man
                 for (int row = 0; row < cas.GetLength(0); row++)
                 {
                     for (int col = 0; col < cas.GetLength(1); col++)
-                    {
+                    {                                                               //Pasarlo a un método
                         if (c[i] == ';') { col--; }
                         else if (c[i] == '0') { cas[row, col] = (Casilla)0; }
                         else if (c[i] == '1') { cas[row, col] = (Casilla)1; }
@@ -162,15 +123,124 @@ namespace Pac_man
 
                 //Inicialización del random
                 rnd = new Random(100);
+
+                
             }
+            public void tabl() /////?????????
+            {
+
+            }
+        }
+
+        static char[] LecturaDeFichero(string file)
+        {
+            StreamReader levels;
+
+            levels = new StreamReader(file);
+
+            //Variables del tamaño del nivel para la creación de "Tablero"
+            int filas = 0;
+            int columnas;
+
+            //String auxiliar para la lectura del nive
+            string end = "";
+            file = "";
+            //Booleano para el control del bucle
+            bool finish = false;
+
+            //El bucle va leyendo cada fila del archivo en "end" y la suma a "file", cuando end encuentre "null" querra decir que no hay más lineas en el archivo
+            while (!finish)
+            {
+                end = levels.ReadLine();
+
+                file += end;
+                file += ";";
+
+                if (end == null) { finish = true; }
+            }
+
+            levels.Close();
+
+            //Creo un array auxiliar para calcular las columnas
+            char[] c;
+
+            c = file.ToCharArray();
+
+            //Limpio el array de espacios vacios
+            string limpio = "";
+            int j = 0;
+            while (j < c.Length)
+            {
+                if (c[j] == ' ') { j++; }
+                else { limpio += c[j]; j++; }
+            }
+
+            c = limpio.ToCharArray();
+
+            return c;
+        }
+
+        static int Filas (char[] c)
+        {
+            int filas = 0;
+
+            int i = 0;
+            bool tamaño = false;
+            while (!tamaño)
+            {
+                if (c[i] == ';' && c[i+1] == ';') { tamaño = true; filas++; }
+                else if(c[i] == ';') { filas++; i++; }
+                else { i++; }
+            }
+            return filas;
+        }
+
+        static int Columnas(char[] c)
+        {
+            //Este bucle recorre el array auxiliar "c" y suma uno a "columnas" y 
+            //cuando encuentra el primer ";" significa que ese es el tamaño de columnas para crear "Tablero"
+            int columnas = 0;
+
+            bool tamaño = false;
+            while (!tamaño)
+            {
+                if (c[columnas] == ';') { tamaño = true; }
+                else { columnas++; }
+            }
+            
+
+            return columnas;
         }
 
 
 
         static void Main(string[] args)
         {
+            string file = "level00.dat";
+            Tablero tab;
+
+            tab = new Tablero(file);
+
+
+            //Dibuja(tab);
+            int alsjda = 0;
             
+
         }
+
+        /*static void Dibuja(Tablero tab)
+        {
+            for (int row = 0; row < tab; row++)
+            {
+                for (int col = 0; col < tab.cas.GetLength(1); col++)
+                {
+
+                }
+            }
+        }*/
+
+
+
 
        
     }
